7677d5625349dbb52aa99911acd884aa8e71b60c
DAKnell <2254566697@qq.com>
2025-07-13 00:35:13 +0800

    proj0-hard
    2048

--- proj0_hardmode/src/game2048logic/GameLogic.java
+++ proj0_hardmode/src/game2048logic/GameLogic.java
@@ -22,13 +22,43 @@ public class GameLogic {
             // code in this method. You will want to write
             // helper methods. And those helper methods should
             // have helper methods.
+            tiltUp(board);
             return;
         } else if (side == Side.EAST) {
+            rotateLeft(board);
+            tiltUp(board);
+            rotateRight(board);
             return;
         } else if (side == Side.WEST) {
+            rotateRight(board);
+            tiltUp(board);
+            rotateLeft(board);
             return;
         } else { // SOUTH
+            rotateRight(board);
+            rotateRight(board);
+            tiltUp(board);
+            rotateRight(board);
+            rotateRight(board);
             return;
+        }
+    }
+    public static void tiltUp(int[][] board){
+        for(int i=0;i<board[0].length;++i){
+            int m=0;
+            for(int j=1;j<board.length;++j){
+                int k=j-1;
+                while(k>=m && board[k][i]==0){
+                    board[k][i]=board[k+1][i];
+                    board[k+1][i]=0;
+                    k--;
+                }
+                if(k>=m&&board[k][i]==board[k+1][i]){
+                    board[k][i]*=2;
+                    board[k+1][i]=0;
+                    m=k+1;
+                }
+            }
         }
     }
 }
